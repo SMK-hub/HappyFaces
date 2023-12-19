5b25448036884207b4c8687c86627a0e8bae18b2
SMK-hub <75328575+SMK-hub@users.noreply.github.com>
2023-12-20 00:26:20 +0530

    Delete HappyFace directory

--- HappyFace/src/main/java/com/example/Demo/LogService/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.Demo.LogService;
-
-import com.example.Demo.LogModel.Admin;
-
-public interface UserService {
-    String registerUser(Admin user);
-    boolean loginUser(String email, String password);
-}
