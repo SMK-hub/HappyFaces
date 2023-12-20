13eb7be6b2e28465d309b18c8f1ac1d71f2b56ab
SMK-hub <75328575+SMK-hub@users.noreply.github.com>
2023-12-20 17:34:05 +0530

    Delete Train-1 directory

--- Train-1/src/main/java/com/example/Demo/AdminServices/AdminService.java
+++
@@ -1,8 +0,0 @@
-package com.example.Demo.AdminServices;
-
-import com.example.Demo.Model.Admin;
-
-public interface AdminService {
-    String registerUser(Admin user);
-    boolean loginUser(String email, String password);
-}
--- Train-1/src/main/java/com/example/Demo/LogRepo/EventsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Demo.LogRepo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.Demo.Model.Events;
-
-public interface EventsRepository extends MongoRepository<Events, String>{
-
-}
