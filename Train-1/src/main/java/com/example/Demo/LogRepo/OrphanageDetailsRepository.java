d142f1f15b64aa94c4c51f199b7880409f883281
SMK-hub <75328575+SMK-hub@users.noreply.github.com>
2023-12-22 16:53:13 +0530

    Delete Train-1 directory

--- Train-1/src/main/java/com/example/Demo/LogRepo/OrphanageDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Demo.LogRepo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.Demo.Model.OrphanageDetails;
-
-public interface OrphanageDetailsRepository extends MongoRepository<OrphanageDetails, String>{
-
-}
