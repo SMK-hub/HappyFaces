76c30789e03c1f19cc3680cf8685ae97b6cf87b9
SMK-hub <75328575+SMK-hub@users.noreply.github.com>
2023-12-27 11:46:16 +0530

    Delete Train-1 directory

--- Train-1/src/main/java/com/example/Demo/Repository/DonationsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Demo.Repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.Demo.Model.Donations;
-
-public interface DonationsRepository extends MongoRepository<Donations, String>{
-
-}
--- Train-1/src/main/java/com/example/Demo/Repository/RequirementsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Demo.Repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.Demo.Model.Requirement;
-
-public interface RequirementsRepository extends MongoRepository<Requirement,String>{
-
-}
