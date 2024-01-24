d7877b9f3606315b9fbecb2535cda399a68881e9
SMK <selvamuthukannan@gmail.com>
2024-01-24 19:31:58 +0530

    updated dashboard admin dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,9 +13,11 @@ import SignInDonor from './components/All_js/SignInDonor';
 import SignUpAdmin from './components/All_js/SignUpAdmin';
 import SignUpOrphanage from './components/All_js/SignUpOrphanage';
 import SignUpDonor from './components/All_js/SignUpDonor';
+
 import DashboardAdmin from './components/Admin_Dashboard/DashboardAdmin';
 import DashboardOrphanage from './components/Orphanage_Dashboard/DashboardOrphanage';
 import DashboardDonor from './components/All_js/DashboardDonor';
+
 import OrphanageHome from './components/Orphanage_Dashboard/components/OrphanageHome';
 import { Container } from '@mui/material';
 import MyContainer from './components/Orphanage_Dashboard/components/Container/Container';
@@ -24,7 +26,6 @@ import EventTable from './components/Orphanage_Dashboard/components/Event/Events
 import Sidebar from './components/Orphanage_Dashboard/components/Sidebar';
 import RightSide from './components/Orphanage_Dashboard/components/RigtSide/RightSide';
 import PaymentDashboard from './components/Orphanage_Dashboard/components/Payments/Payment';
-
 
 
 const App = () => {
@@ -42,9 +43,11 @@ const App = () => {
         <Route path="/signup/admin" element={<SignUpAdmin />} />
         <Route path="/signup/orphanage" element={<SignUpOrphanage />} />
         <Route path="/signup/donor" element={<SignUpDonor />} />
+
         <Route path="/admin-dashboard" element={<DashboardAdmin/>}/>
         <Route path='/orphanage-dashboard' element={<DashboardOrphanage/>}/>
         <Route path="/donor-dashboard" element={<DashboardDonor/>}/>
+
         <Route path="/OrphanageHome" element={<OrphanageHome />} />
         <Route path="/OrphanageHome/details" element={<MyContainer/>}/>
         <Route path="/OrphanageHome/profile" element={<Profile/>}/>
